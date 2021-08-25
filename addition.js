82a3ccab7269165fcff0a117dcb3d4832bdc4444
syroxceo <82676910+syroxceo@users.noreply.github.com>
2021-08-25 10:46:47 -0700

    Add files via upload

---
+++ addition.js
@@ -0,0 +1,23 @@
+function add(){
+var result= Number(document.getElementById("n1").value) + Number(document.getElementById("n2").value);
+                                                                 
+  window.alert("answer is "+result);
+}
+
+function multiply(){
+var result= Number(document.getElementById("n1").value) * Number(document.getElementById("n2").value);
+                                                                 
+  window.alert("answer is "+result);
+}
+
+function subtract(){
+var result= Number(document.getElementById("n1").value) - Number(document.getElementById("n2").value);
+                                                                 
+  window.alert("answer is "+result);
+}
+
+function divide(){
+var result= Number(document.getElementById("n1").value) / Number(document.getElementById("n2").value);
+                                                                 
+  window.alert("answer is "+result);
+}
